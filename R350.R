152b4ee09b1a99c3da0a95d6ff6b7f4ddeec0031
Brian Clare <brianclare.math@gmail.com>
2018-04-27 14:17:11 -0400

    cleaned up

--- R350.R
+++ R350.R
@@ -3,10 +3,6 @@
 # http://blog.revolutionanalytics.com/2017/09/altrep-preview.html
 # https://svn.r-project.org/R/branches/ALTREP/ALTREP.html
 
-n <- 10000000
-x <- rnorm(n)
-y <- rnorm(n)
-
 
 # note: you can use scientific notation in R with 'e'
 # 1e6 means 1x10^6 = 1000000
